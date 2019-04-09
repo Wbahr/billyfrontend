5bee052917865d9b7ccb478843c62a82b79e82f4
Bobby Panczer <bobbypanczer@gmail.com>
2019-04-09 15:34:10 -0400

    Add react datepicker

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
+// import Screen from "./screens/mainScreen"
+// import DatePicker from "react-datepicker"
 
 const App = () => {
-  return <div>Hello React!</div>;
+  return(
+    <div>
+      <h1>Hello!</h1>
+      {/* <DatePicker
+          selected={this.state.startDate}
+          onChange={this.handleChange}
+      />
+      <DatePicker
+        selected={this.state.endDate}
+        onChange={this.handleChange}
+      /> */}
+    </div>
+  )
 };
 
 ReactDOM.render(<App />, document.getElementById("index"));
