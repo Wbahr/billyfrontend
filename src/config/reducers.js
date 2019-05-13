0ba856d56ef3942965a694b650efd9ec85517186
Bobby Panczer <bobbypanczer@gmail.com>
2019-05-12 20:10:16 -0400

    install eslint and autofix

--- src/config/reducers.js
+++ src/config/reducers.js
@@ -3,5 +3,5 @@ import { combineReducers } from 'redux'
 import rmaReducer from '../uiComponents/RMA/redux/reducer'
 
 export default combineReducers({
-  returnMaterialAuthorization: rmaReducer
+	returnMaterialAuthorization: rmaReducer
 })
--- src/uiComponents/RMA/redux/actionConsts.js
+++ src/uiComponents/RMA/redux/actionConsts.js
@@ -1,5 +1,5 @@
-export const REQUEST_TESTING = "REQUEST_TESTING"
-export const RECEIVE_TESTING = "RECEIVE_TESTING"
+export const REQUEST_TESTING = 'REQUEST_TESTING'
+export const RECEIVE_TESTING = 'RECEIVE_TESTING'
 
 export const requestTesting = () => ({ type: REQUEST_TESTING })
 export const receiveTesting = text => ({ type: RECEIVE_TESTING , text})
