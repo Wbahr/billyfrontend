6c3e78e66c3e5114e2126ec3692bb0c0b9ccd4dc
Bobby Panczer <bobbypanczer@gmail.com>
2019-05-16 21:24:42 -0400

    generalize api calls

--- src/api-temp/apiCalls.js
+++ src/api-temp/apiCalls.js
@@ -1,8 +1,10 @@
-const url = 'https://preprodapi.airlinehyd.com'
+import Cookies from 'js-cookie'
+
+const url = 'https:' + window.apiBaseUrl
+const token = Cookies.get('b2bApiToken')
 
 export function getInvoice(invoiceNum) {
 	// Default options are marked with *
-	const token = 'hVa-Olr_iqUeJAp9ZlVrEm6b4pOT_6KimgT74xGOaoO1IngU7vdK4zarsx4i6ajSEyo1ycePpoCYcTnbJgydK1_3ughRrNxKmE9lM-6I5xjccF_dW1i9siLAa2xjD-923dZ8y6OFjTwaYSsAONLT4CtscjpPEv_CYzzkvnnlpDShpEve00hd_d-9R4RTmvVbqzE4O_dMcCvVZ0-YBFZGNu1bZTACZ2Tt-MHLs61w0ILPHjg9qmimDdYFrA72XJDIyE8gjcnw2Ob6zqq-FU7zGCZY20zGPklhvoFqcmqMGF4yNP1ZD0aW4zoBdGyDlQeJhgjUaQpeE-KLb2pcTabvGDe8a4rVemDJeRpR0B02zEc7yjCHu2oem1RVRUze7sO3Puf2OdMrisPyf4JMjvQthb-uT0tjHckuGG6dRODjfVfr74QJK0jlAMMJzj8t9SWWE-MrM5DAzDxPZnOkyiRPcthpiM7DPXyFbqng6bFfJ3byb2HcPsYO0S4bDF93iBFUVJdEuBUG_ZfunWpI8yYjYylxKXusZs2sXkuMpzhiMBQttLP8WXdGCaufqDvrnMinR9tLRX_2ryXdu-s7YJGlnmaNf9tY8arHNcuHoWnIFzO0inlw752QW6YpQOLT7fnH'
 	const endpoint = `/corvus/myaccount/get/invoices/${invoiceNum}?outputType=json`
 	return fetch(url + endpoint, {
 		method: 'GET', // *GET, POST, PUT, DELETE, etc.
@@ -29,7 +31,6 @@ export function getInvoice(invoiceNum) {
 
 // UPDATE THIS ONCE I HAVE AN RMA
 export function postRMA(data) {
-	const token = 'hVa-Olr_iqUeJAp9ZlVrEm6b4pOT_6KimgT74xGOaoO1IngU7vdK4zarsx4i6ajSEyo1ycePpoCYcTnbJgydK1_3ughRrNxKmE9lM-6I5xjccF_dW1i9siLAa2xjD-923dZ8y6OFjTwaYSsAONLT4CtscjpPEv_CYzzkvnnlpDShpEve00hd_d-9R4RTmvVbqzE4O_dMcCvVZ0-YBFZGNu1bZTACZ2Tt-MHLs61w0ILPHjg9qmimDdYFrA72XJDIyE8gjcnw2Ob6zqq-FU7zGCZY20zGPklhvoFqcmqMGF4yNP1ZD0aW4zoBdGyDlQeJhgjUaQpeE-KLb2pcTabvGDe8a4rVemDJeRpR0B02zEc7yjCHu2oem1RVRUze7sO3Puf2OdMrisPyf4JMjvQthb-uT0tjHckuGG6dRODjfVfr74QJK0jlAMMJzj8t9SWWE-MrM5DAzDxPZnOkyiRPcthpiM7DPXyFbqng6bFfJ3byb2HcPsYO0S4bDF93iBFUVJdEuBUG_ZfunWpI8yYjYylxKXusZs2sXkuMpzhiMBQttLP8WXdGCaufqDvrnMinR9tLRX_2ryXdu-s7YJGlnmaNf9tY8arHNcuHoWnIFzO0inlw752QW6YpQOLT7fnH'
 	const endpoint = '/corvus/orders/rma'
 	return fetch(url + endpoint, {
 		method: 'POST',
