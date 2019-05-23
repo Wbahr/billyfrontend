36d1bbb4c5748b2a1f0585b715967eef98a8f44d
Bobby Panczer <bobbypanczer@gmail.com>
2019-05-23 11:37:37 -0400

    update styling

--- src/uiComponents/SMCProductConfigurator/productConfigSearch.js
+++ src/uiComponents/SMCProductConfigurator/productConfigSearch.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import styled from 'styled-components'
 import smclogo from '../../imgs/SMCLogo.png'
+import Loader from '../_common/loader'
+
 
 const DivRow = styled.div`
   display: flex;
@@ -8,16 +10,51 @@ const DivRow = styled.div`
 `
 const DivContainer = styled.div`
   display: flex;
+`
+
+const DivResultsSummary = styled.div`
+  display: flex;
+  border-bottom: 1px #999 solid;
+  align-items: center;
+  justify-content: space-between;
+`
+
+const DivColumn = styled.div`
+  display: flex;
   flex-direction: column;
+`
+
+const DivColumn1 = styled(DivColumn)`
+  flex-grow: 4;
 `
 
 const DivResultsContainer = styled.div`
   display: flex;
+  flex-direction: column;
+  max-width: 600px;
 `
 
-const DivSearchResultsContainer = styled.div`
+const DivItemsContainer = styled.div`
   display: flex;
   flex-direction: column;
+`
+
+const DivResultItem = styled.div`
+  font-family: verdana;
+  font-size: 14px;
+  color: #337ab7;
+  cursor: pointer;
+  display: flex;
+  height: 52px;
+  border-bottom: 1px solid #999;
+  :hover {
+    font-weight: 600;
+  }
+`
+
+const PresultSummary = styled.p`
+  font-family: verdana;
+  font-weight: 400;
 `
 
 const P = styled.p`
@@ -28,11 +65,23 @@ const P = styled.p`
   max-width: 700px;
 `
 
+const Alink = styled.a`
+  cursor: pointer;
+  font-family: verdana;
+  font-size: 12px;
+  color: darkblue;
+  text-decoration: underline;
+`
+
 const H1 = styled.h1`
   font-family: verdana;
   color: #777
   font-size: 16px;
   margin: 10px 0 6px 0;
+`
+
+const Img = styled.img`
+  padding: 0 8px;
 `
 
 const InputSearch = styled.input`
@@ -77,40 +126,63 @@ const ButtonSearch = styled.button`
   }
 `
 
-const searchResults = [
-  // {
-  //   'searchTerm': 'Actuator with double fittings - Aluminum',
-  //   'img': '/this/is/where/the/iamge/is.jpg',
-  //   'link': '/link/to/our/site'
-  // },
-  // {
-  //   'searchTerm': 'Dual Connector with orange nossle',
-  //   'img': '/this/is/where/the/iamge/is2.jpg',
-  //   'link': '/link/to/our/site2'
-  // }
-]
+const searchResultsR = {
+  'searchTerm': 'nossels',
+  'searchResults': [
+    {
+      'searchTerm': 'Actuator with double fittings - Brass',
+      'img': 'https://content2.smcetech.com/image/small/3001D_US.jpg',
+      'link': 'https://www.google.com'
+    },
+    {
+      'searchTerm': 'Dual Connector with orange nossle',
+      'img': 'https:////content2.smcetech.com/image/small/3001C_US.jpg',
+      'link': 'https://www.bing.com'
+    }
+  ]
+}
 
 class ProductConfigSearch extends React.Component {
   state = {
     searchResults: {},
-    searchTerm: ''
+    searchTerm: '',
+    searching: false
+  }
+
+  handleSearchClick = () => {
+   const {
+     searchTerm,
+     searching
+   } = this.state
+
+    if(!searching && searchTerm.length === 0) {
+      this.setState({searching: true})
+      getSMCParts(searchTerm).then(
+      (response) => {
+        if(response.ok) {
+          this.setState({searchResults: response.searchResults, searching: false})
+        } else {
+          this.setState({searching: false})
+        }
+      })
+    }
   }
 
   render(){
     const {
       searchResults,
-      searchTerm
+      searchTerm,
+      searching
     } = this.state
 
     let searchBar = (
       <DivRow>
-        <img src={smclogo} alt='smc-logo' height='50px' width='150px'/>
         <InputSearch
           placeholder='Search for SMC Products...'
           value={searchTerm}
           onChange={(e) =>{this.setState({searchTerm: e.target.value})}}
         />
-        <ButtonSearch>
+        <ButtonSearch onClick={this.handleSearchClick}>
           Search
         </ButtonSearch>
       </DivRow>
@@ -125,23 +197,42 @@ class ProductConfigSearch extends React.Component {
     )
 
     let searchResultsComponent
-    let resultCount = searchResults.length
+    let resultCount = searchResultsR.searchResults.length
+    let Items = _.map(searchResultsR.searchResults, (result)=>
+      <DivResultItem onClick={()=>{location.replace(result.link)}}>
+        <Img src={result.img} width='50px' height='50px' />
+        <p>{result.searchTerm}</p>
+      </DivResultItem>
+    )
+
     if (resultCount > 0) {
+      let resultText = resultCount === 1 ? 'result' : 'results'
       searchResultsComponent = (
         <DivResultsContainer>
-          <P>Back to Categories</P>
-          <DivSearchResultsContainer>
-            <p>{`${resultCount} results found for your search of '${searchTerm}'.`}</p>
-          </DivSearchResultsContainer>
+          <DivResultsSummary>
+            <PresultSummary>{`Your search for '${searchResultsR.searchTerm}' returned ${resultCount} ${resultText}.`}</PresultSummary>
+            <Alink href='https://preprod.airlinehyd.com/customer/aihyco/smc/pages/smcusa.aspx' >Back to Categories</Alink>
+          </DivResultsSummary>
+          <DivItemsContainer>
+            {Items}
+          </DivItemsContainer>
         </DivResultsContainer>
       )
     }
     return (
+      <>
       <DivContainer>
-        {searchBar}
-        {brandDescription}
-        {/*{searchResultsComponent}*/}
+        <DivColumn>
+          <img src={smclogo} alt='smc-logo' height='50px' width='150px'/>
+        </DivColumn>
+        <DivColumn1>
+          {searchBar}
+          {resultCount !== 0 && searchResultsComponent}
+          {searching && <Loader />}
+        </DivColumn1>
       </DivContainer>
+        {resultCount === 0 && brandDescription}
+      </>
     )
   }
 }
