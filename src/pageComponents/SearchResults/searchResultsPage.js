ec3891026974162af855cb6f900f6b1217d1dda5
Bobby Panczer <bobbypanczer@gmail.com>
2019-10-08 10:25:24 -0400

    set up general frontend structure

---
+++ src/pageComponents/SearchResults/searchResultsPage.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import styled from 'styled-components'
+import ContentScreen from '../../containerComponents/contentScreen'
+
+const 
+class SearchResultsPage extends React.Component {
+  render(){
+    return(
+      <ContentScreen>
+        <div>
+          <p>Test Div</p>
+        </div>
+      </ContentScreen>
+    )
+  }
+}
+
+export default SearchResultsPage
