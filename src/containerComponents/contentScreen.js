7391806222a8ced1b5bf1706ac47c6c498bb06cf
Bobby Panczer <bobbypanczer@gmail.com>
2019-10-09 09:24:24 -0400

    Result sorting and show dropdowns

--- src/containerComponents/contentScreen.js
+++ src/containerComponents/contentScreen.js
@@ -3,18 +3,23 @@ import styled from 'styled-components'
 import { ItemResult } from '../pageComponents/SearchResults/uiComponents/itemResult'
 import ResultsSummary from '../pageComponents/SearchResults/uiComponents/resultsSummary'
 import ResultsSearch from '../pageComponents/SearchResults/uiComponents/resultsSearch'
-
+import ResultsFilter from '../pageComponents/SearchResults/uiComponents/resultsFilter'
 const ContentScreenContainer = styled.div`
   display: flex;
   flex-direction: column;
   max-width: 1200px;
   margin: 100px auto;
 `
+
+const Div = styled.div`
+  display: flex;
+`
+
 export default function ContentScreen(props) {
   return(
     <ContentScreenContainer>
       <ResultsSummary /> 
-      <ResultsSearch />
+      <Div><ResultsSearch /><ResultsFilter /></Div>
       <ItemResult/>
     </ContentScreenContainer>
   )
