6a9391539ba21314590fe6b8f1550aa27470a430
Bobby Panczer <bobbypanczer@gmail.com>
2019-10-11 15:06:55 -0400

    clean up code

--- src/pageComponents/SearchResults/searchResultsPage.js
+++ src/pageComponents/SearchResults/searchResultsPage.js
@@ -1,6 +1,5 @@
 import React from 'react'
 // import styled from 'styled-components'
-import ContentScreen from '../../layoutComponents/contentScreen'
 import ItemResult from './uiComponents/itemResult'
 import ResultsSearch from './uiComponents/resultsSearch'
 
