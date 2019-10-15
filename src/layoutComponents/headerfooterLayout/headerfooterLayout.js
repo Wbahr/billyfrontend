30ffbe6a93eb555a136f44d8e2630241a9408331
Bobby Panczer <bobbypanczer@gmail.com>
2019-10-15 16:08:02 -0400

    clean up code

--- src/layoutComponents/headerfooterLayout/headerfooterLayout.js
+++ src/layoutComponents/headerfooterLayout/headerfooterLayout.js
@@ -18,18 +18,6 @@ const ContentScreenContainer = styled.div`
   margin: 28px auto;
 `
 
-// const Temp = styled.div`
-//   margin-left: 257px;
-// `
-
-// const Category = styled.div`
-//   width: 125px;
-//   height: 125px;
-//   background-image: 
-//   linear-gradient(to bottom, rgba(245, 246, 252, 0.52), rgba(117, 19, 93, 0.73)),
-//   url('../imgs/airline/hydraulics.jpg');
-//   `
-
 export default function HeaderFooterLayout(props) {
   console.log(props)
   return(
