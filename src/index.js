49d2d51b8fba16e74ffbedfb6d59f5e312185da0
John George <JGeorge@airlinehyd.com>
2019-10-21 16:50:42 -0400

    Implemented Apollo for Graphql

--- src/index.js
+++ src/index.js
@@ -7,6 +7,8 @@ import { createBrowserHistory } from "history"
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fab } from '@fortawesome/free-brands-svg-icons'
 import { faCheckSquare, faCoffee, faPhoneAlt, faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons'
+import { ApolloProvider } from "@apollo/react-hooks";
+import ApolloClient from 'apollo-boost'
 
 library.add(fab, faCheckSquare, faCoffee, faPhoneAlt, faChevronLeft, faChevronRight)
 
@@ -14,9 +16,17 @@ const customHistory = createBrowserHistory();
 // import MainScreen from './containerComponents/mainScreen'
 // import SearchResults from './pageComponents/SearchResults/searchResultsPage'
 
+const apolloClient = new ApolloClient({
+  uri: process.env.API_URL
+})
+
+
 ReactDOM.render(
-  <BrowserRouter history={customHistory}>
-    <Switch />
-  </BrowserRouter>
+  <ApolloProvider client={apolloClient}>
+    <BrowserRouter history={customHistory}>
+      <Switch />
+    </BrowserRouter>
+  </ApolloProvider>
+  
   , document.getElementById('index')
 )
