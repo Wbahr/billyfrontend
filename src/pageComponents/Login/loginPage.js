8ce2a689af211f3680cf1df10e9b751fc56652fa
Bobby Panczer <bobbypanczer@gmail.com>
2019-10-22 14:21:11 -0400

    Rough login page

--- src/pageComponents/Login/loginPage.js
+++ src/pageComponents/Login/loginPage.js
@@ -1,25 +1,103 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import styled from 'styled-components'
+import AirlineLogoCircle from '../../imgs/airline/airline_circle_vector.png'
+
 
 const LoginPageContainer = styled.div`
   display: flex;
   flex-direction: column;
+  align-items: center;
   height: 100%;
-  max-width: 1200px;
+  width: 350px;
   margin: 28px auto;
-  justify-content: space-between;
   flex-grow: 99;
 `
 
-class LoginPage extends React.Component {
+const DivInput = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 20px;
+`
 
-  render(){
-    return(
-      <LoginPageContainer>
-        <p>Login</p>
-      </LoginPageContainer>
-    )
+const P = styled.p`
+  margin: 20px 0;
+  border-bottom: 1px #ddd solid;
+  width: 100%;
+  text-align: center;
+  font-size: 20px;
+`
+
+const Label = styled.label`
+  color: grey;
+  font-size: 14px;
+  font-weight: 300;
+  padding-left: 4px;
+  margin: 0;
+`
+
+const Input = styled.input`
+  width: 300px;
+  height: 42px;
+  padding: 0 8px;
+`
+
+const A = styled.a`
+  cursor: pointer;
+  margin-top: 8px;
+  color: grey;
+  &:hover{
+    color: blue;
   }
-}
+`
 
-export default LoginPage
+const Button = styled.button`
+  background-color: black;
+  color: white;
+  border: none;
+  font-size: 20px;
+  padding: 8px 16px;
+  &:hover{
+    background-color: #DB1633;
+    transition: background-color 300ms;
+  }
+`
+
+export default function LoginPage() {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [forgotPassword, setforgotPassword] = useState(false)
+
+
+  useEffect(() => {
+  },[email, password])
+
+  function handleSignin(){
+    console.log('signing in')
+  }
+
+  function setToken() {
+    console.log('setting token')
+  }
+
+  function handleForgotPassword(){
+    console.log('forgot password')
+  }
+
+  return(
+    <LoginPageContainer>
+      <img src={AirlineLogoCircle} height='75px' onClick={()=> console.log('go home')}/>
+      <P>Airline Hydraulics Login</P>
+      <DivInput>
+        <Label for='email'>Email Address</Label>
+        <Input id='email' onChange={(e)=>setEmail(e.target.value)} value={email}/>
+      </DivInput>
+      <DivInput>
+        <Label for='password'>Password</Label>
+        <Input id='password' type='password' onChange={(e)=>setPassword(e.target.value)} value={password}/>
+      </DivInput>
+      <Button onClick={()=>handleSignin()}>Sign In</Button>
+      <A onClick={()=>handleForgotPassword()}>Forgot your Password?</A>
+      <A onClick={()=>handleForgotPassword()}>Create an Account</A>
+    </LoginPageContainer>
+  )
+}
--- src/pageComponents/Signup/signupPage.js
+++ src/pageComponents/Signup/signupPage.js
@@ -1,25 +1,54 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import styled from 'styled-components'
 
 const SignupPageContainer = styled.div`
   display: flex;
   flex-direction: column;
+  align-items: center;
   height: 100%;
   max-width: 1200px;
   margin: 28px auto;
-  justify-content: space-between;
   flex-grow: 99;
 `
 
-class SignupPage extends React.Component {
+const DivInput = styled.div`
+  display: flex;
+  flex-direction: column;
+`
+export default function SignupPage() {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
 
-  render(){
-    return(
-      <SignupPageContainer>
-        <p>Signup Page</p>
-      </SignupPageContainer>
-    )
+  useEffect(() => {
+  },[email, password])
+
+  function handleSignin(){
+    console.log('signing in')
   }
-}
 
-export default SignupPage
+  function setToken() {
+    console.log('setting token')
+  }
+
+  function handleForgotPassword(){
+    console.log('forgot password')
+  }
+
+  return(
+    <SignupPageContainer>
+      <p>Logo</p>
+      <p>Login</p>
+      <DivInput>
+        <label for='email'>Email Address</label>
+        <input id='email' onChange={(e)=>setEmail(e.target.value)} value={email}/>
+      </DivInput>
+      <DivInput>
+        <label for='password'>Password</label>
+        <input id='password' type='password' onChange={(e)=>setPassword(e.target.value)} value={password}/>
+      </DivInput>
+      <a onClick={()=>handleForgotPassword()}>Forgot your Password?</a>
+      <a onClick={()=>handleForgotPassword()}>Don't have an Account? Create one here</a>
+      <button onClick={()=>handleSignin()}>Sign In</button>
+    </SignupPageContainer>
+  )
+}
