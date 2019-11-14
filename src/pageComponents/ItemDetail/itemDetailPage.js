7310bf5f3ad9c9a38e55d961ee47632eb0761c18
Bobby Panczer <bobbypanczer@gmail.com>
2019-11-13 19:22:25 -0500

    Add Accessory Item to Product Detail Page

--- src/pageComponents/ItemDetail/itemDetailPage.js
+++ src/pageComponents/ItemDetail/itemDetailPage.js
@@ -4,7 +4,7 @@ import styled from 'styled-components'
 import { useQuery } from '@apollo/react-hooks'
 import gql from 'graphql-tag'
 import Loader from '../_common/loader'
-
+import { AccessoryItem } from './uiComponents/accessoryItem'
 
 //This grabs every piece of available data. Remove unneeded fields.
 const GET_ITEM_BY_ID = gql`
@@ -116,14 +116,15 @@ const ItemDetailPageContainer = styled.div`
 `
 
 const DivPhoto = styled.div`
-    width: 400px;
-    height: 400px;
-    margin: 0px 8px;
+  width: 400px;
+  height: 400px;
+  margin: 0px 8px;
 `
 
 const DivDetails = styled.div`
+  width: 500px;
+  padding: 0 32px;
   flex-grow: 99;
-  margin: 0 32px;
 `
 
 const DivPurchaseInfo = styled.div`
@@ -145,9 +146,34 @@ const Div = styled.div`
   align-content: center;
 `
 
+const Row = styled.div`
+  display: flex;
+  width: 100%;  
+  align-items: flex-end;
+`
+
+const P = styled.p`
+  margin: 0;
+`
+
+const Pbold = styled(P)`
+  font-weight: bold;
+`
+
+const H4 = styled.h5`
+  margin: 12px 0 0 0;
+  font-weight: 600;
+`
+
+const DivSection = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-left: 24px;
+`
+
 const H2ItemTitle = styled.h2`
   font-size: 25px;
-  font-weight: 500;
+  font-weight: 600;
 `
 const PManufacturer = styled.p`
   font-size: 14px;
@@ -155,7 +181,6 @@ const PManufacturer = styled.p`
 
 const PItemExtendedDescription = styled.p`
   font-size: 16px;
-  color: slate;
 `
 
 const H3ItemSection = styled.h3`
@@ -196,6 +221,29 @@ const ButtonBlack = styled.button`
   &:active{
     opacity: 1;
   }
+`
+
+const Table = styled.table`
+  margin: 0 16px;
+  table-layout: fixed;
+  width: 100%;
+`
+
+const TR = styled.tr`
+  border-bottom: 1px whitesmoke solid;
+`
+
+const TD = styled.td`
+  word-wrap:break-word;
+  padding-right: 8px;
+`
+
+const Pprice = styled.p`
+  color: #328EFC;
+  font-size: 18px;
+  font-weight: 700;
+  padding-right: 4px;
+  margin: 0;
 `
 
 export default function ItemDetailPage(){
@@ -241,7 +289,13 @@ export default function ItemDetailPage(){
 
     let TechSpecItems = item.techSpec.map(elem => {
       return(
-        <li>{elem.name} {elem.value}</li>
+        <TR><TD>{elem.name}</TD><TD>{elem.value}</TD></TR>
+      )
+    })
+
+    let ItemLinks = item.itemLink.map(elem => {
+      return(
+        <a href={elem.linkPath}>{elem.title}</a>
       )
     })
 
@@ -252,10 +306,25 @@ export default function ItemDetailPage(){
     )
     
     let TechSpecs = (
-      <ul>
-        {TechSpecItems}
-      </ul>
+      <div>
+        <Table>
+          {TechSpecItems}
+        </Table>
+      </div>
     )
+
+    let Links = (
+      <DivSection>
+        {ItemLinks}
+      </DivSection>
+    )
+
+    let AccessoryItems = item.itemAssociationInvMastU.map(elem => {
+      return(
+        <AccessoryItem associatedItemId={elem.associatedInvMastUid}/>
+      )
+    })
+
     return(
       <ItemDetailPageContainer>
         <DivPhoto>
@@ -265,20 +334,31 @@ export default function ItemDetailPage(){
           <H2ItemTitle>{item.itemDesc}</H2ItemTitle>
           <PManufacturer>Manufacturer: {item.itemCode}</PManufacturer>
           <hr/>
-          {item.availability === 0 ? <p>{item.availabilityMessage}</p> : <p>{`Availability: ${item.availability}`}</p>}
-          <p>{`$${item.anonPrice} /each`}</p>
-          <p>{item.mfgPartNo}</p>
-          <p>{item.itemCode}</p>
-          <p>AHC#{item.invMastUid}</p>
+          <Row>
+            <Pprice>{`Price: $${item.anonPrice}.00`}</Pprice>
+            {item.availability === 0 ? <Pbold>{item.availabilityMessage}</Pbold> : <Pbold>{`Availability: ${item.availability}`}</Pbold>}
+          </Row>
           <PItemExtendedDescription>{item.extendedDesc}</PItemExtendedDescription>
-          <H3ItemSection>Features</H3ItemSection>
+          <H4>Product Specifications</H4>
+          <DivSection>
+            <P>Manufacturer Part #: {item.mfgPartNo}</P>
+            <P>Manufacturer Item Code: {item.itemCode}</P>
+            <P>AHC Part #: {item.invMastUid}</P>
+          </DivSection>
+          <H4>Features</H4>
           {Features}
-          <H3ItemSection>Tech Specs</H3ItemSection>
+          <H4>Tech Specifications</H4>
           {TechSpecs}
+          <H4>Links</H4>
+          {Links}
+          <H4>Accessory Items</H4>
+          <DivSection>
+            {AccessoryItems}
+          </DivSection>
         </DivDetails>
         <DivPurchaseInfo>
           <Div>
-            <p>{`$${item.anonPrice} /each`}</p>
+            <p>{`$${item.anonPrice} /EA`}</p>
             <label>Qty:</label><input value={1}/>
             <hr/>
             <p>Availability</p>
---
+++ src/pageComponents/ItemDetail/uiComponents/accessoryItem.js
@@ -0,0 +1,302 @@
+import React, { useState, useEffect, useRef} from 'react'
+import { Link } from "react-router-dom"
+import Loader from '../../_common/loader'
+import styled from "styled-components"
+import gql from 'graphql-tag'
+
+const GET_ITEM_BY_ID = gql`
+    query ItemById($associatedItemId: ID){
+        items(invMastUid: $associatedItemId) {
+            anonPrice
+            assembly
+            availability
+            availabilityMessage
+            cBrandId
+            dateCreated
+            dateModified
+            extendedDesc
+            filters
+            hideOnWeb
+            invMastUid
+            itemCode
+            itemDesc
+            listPrice
+            mfgPartNo
+            modelCode
+            p21ItemDesc
+            p21NonWeb
+            popularity
+            preferredSourceLoc
+            relevancy
+            restrictedCustomerCodes
+            rootCategoryUids
+            showPrice
+            supplierId
+            tariff
+            unitSizeMultiple
+            feature {
+                createDate
+                createdBy
+                invMastUid
+                lastModifiedDate
+                modifiedBy
+                sequence
+                text
+                type
+                uid
+              }
+              image {
+                createDate
+                createdBy
+                invMastUid
+                lastModifiedDate
+                modifiedBy
+                path
+                sequence
+                type
+                uid
+              }
+        }
+    }
+`
+
+const DivItemResultContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  width: 320px;
+  height: 350px;
+  margin: 0 8px 20px 8px;
+  padding: 8px 0;
+  border-bottom: 1px grey solid;
+`
+
+const DivPartNumberRow = styled.div`
+  width: 100%;
+  display: flex;
+  color: #000;
+  padding: 0 5px;
+  font-size: 12px;
+  font-family: Arial, sans-serif;
+`
+
+const DivPartNumberRowSpread = styled(DivPartNumberRow)`
+  justify-content: space-between;
+`
+
+const P = styled.p`
+  margin: 0;
+  font-weight: 500;
+  margin: 0 4px;
+`
+
+const Pred = styled(P)`
+  font-weight: 600;
+  margin: 0;
+`
+
+const DivPartDetailsRow = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  flex-grow: 99;
+  background-color: #fff;
+  width: 100%;
+`
+
+const DivPartImg = styled.div`
+  display: flex;
+  width: 150px;
+  height: 150px;
+  background-color: white;
+`
+
+const DivPartDetails = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 4px;
+`
+
+const PpartTitle = styled.p`
+  margin: 0;
+  font-weight: 700;
+  font-size: 15px;
+  color: #000000 !important;
+  height: 45px;
+  overflow: hidden;
+  &:hover{
+    cursor: pointer;
+    color: #328EFC;
+  }
+`
+
+const PpartDesc = styled.p`
+  margin: 0 0 auto 0;
+  font-size: 13px;
+`
+
+const PpartAvailability = styled.p`
+  margin: 0;
+  font-size: 13px;
+`
+
+const DivPartAction = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: auto;
+  padding: 4px 8px;
+  align-items: flex-end;
+`
+
+const ButtonRed = styled.button`
+  background-color: #b51029;
+  color: white;
+  font-weight: 600;
+  border: 0;
+  padding: 4px 8px;
+  box-shadow: 1px 1px 2px #000;
+  margin: 4px auto;
+  &:hover{
+    background-color: rgb(219, 22, 51);
+  }
+  &:active{
+    background-color: #b51029;
+    box-shadow: 0px 0px 1px #000;
+  }
+`
+const ButtonBlack = styled.button`
+  width: max-content;
+  background-color: white;
+  color: #328EFC;
+  font-weight: 600;
+  font-size: 12px;
+  border: 0;
+  margin-top: 4px;
+`
+
+const Div = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`
+
+const DivSpace = styled(Div)`
+  width: 100%;
+  justify-content: space-between;
+  align-items: flex-end;
+  flex-grow: 99;
+`
+
+const InputQuantity = styled.input`
+  width: 50px;
+  height: 25px;
+  margin-left: 4px;
+`
+
+const Pprice = styled.p`
+  color: #328EFC;
+  font-size: 18px;
+  font-weight: 700;
+  padding: 0 4px;
+  margin: 0;
+`
+
+const ACall = styled.a`
+  color: #328EFC;
+  font-weight: 700;
+  padding: 0 4px;
+`
+
+const PBlue = styled.p`
+  cursor: pointer;
+  color: #328EFC;
+  margin: 0;
+  font-size: 13px;
+  padding: 0 4px;
+`
+
+const Img = styled.img`
+  margin: auto;
+  max-height: 100%;
+  max-width: 100%;
+`
+
+export default function AccessoryItem({associatedItemId, history}) {
+  const didMountRef = useRef(false);
+  const [quantity, setQuantity] = useState(1)
+  const [result, setItem] = useState(null)
+  
+  useEffect(() => {
+    if (!didMountRef.current) {
+      const { 
+        loading, 
+        error, 
+        data 
+      } = useQuery(GET_ITEM_BY_ID, {
+        variables: { associatedItemId },
+        onCompleted: result => {
+          if (result.items.length) {
+            setItem(result.items[0])
+          } else {
+            setItem({})
+          }
+        }
+      })
+      didMountRef.current = true
+    }
+  })
+
+  function handleSetQuantity(quantity){
+    if (/^\+?(0|[1-9]\d*)$/.test(quantity) || quantity === ''){
+      setQuantity(quantity)
+    }
+  }
+
+  function handleAddToCart() {
+    if (quantity.length > 0){
+    // addToCart(quantity, frecno)
+    }
+  }
+
+  let imagePath
+  if (_.isNil(result.thumbnail_image_path)){
+    imagePath = 'https://www.airlinehyd.com/images/no-image.jpg'
+  } else {
+    let imagePathArray = result.thumbnail_image_path.split("\\")
+    let imageFile = imagePathArray[imagePathArray.length - 1]
+    imageFile = imageFile.slice(0, -5) + 'l.jpg'
+    imagePath = 'https://www.airlinehyd.com/images/items/' + imageFile
+  }
+
+  if (_.isNil(result)){
+    return(
+      <Loader />
+    )
+  } else {
+    return(
+      <DivItemResultContainer>
+        <DivPartDetailsRow>
+          <DivPartImg>
+            <Img src={imagePath}/>
+          </DivPartImg>
+          <DivPartDetails>
+            <PpartTitle onClick={()=>{history.push(`/product/${result.frecno}`)}}>{result.item_desc}</PpartTitle>
+          </DivPartDetails>
+          <DivPartNumberRow>
+            <PpartAvailability>Airline #: AHC{result.frecno}</PpartAvailability>
+          </DivPartNumberRow>
+          <DivPartNumberRow><PpartAvailability>Availability:</PpartAvailability>
+            {result.availability !== 0 ? <PBlue>{result.availability} -- Locations </PBlue> : <PBlue>{result.availability_message}</PBlue>}
+          </DivPartNumberRow>
+          <DivPartNumberRowSpread>
+            <Div>Quantity:<InputQuantity value={quantity} onChange={(e) => handleSetQuantity(e.target.value)}/></Div>
+            {(!_.isNil(result.anon_price) && result.anon_price !== 0) ? <Div><Pprice>${result.anon_price.toFixed(2)}</Pprice><P>/EA</P></Div> : <ACall href="tel:+18009997378">Call for Price</ACall>}
+          </DivPartNumberRowSpread>
+          <DivSpace>
+            <ButtonRed onClick={handleAddToCart}>Add to Cart</ButtonRed>
+          </DivSpace>
+        </DivPartDetailsRow>
+      </DivItemResultContainer>
+    )
+  }
+}
