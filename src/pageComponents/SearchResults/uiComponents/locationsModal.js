fe61ef4b2d4310879ee3a0d368f27e7d6f7968cd
Bobby Panczer <bobbypanczer@gmail.com>
2019-12-02 15:45:53 +0100

    Only render rows with stock greater than 0

--- src/pageComponents/SearchResults/uiComponents/locationsModal.js
+++ src/pageComponents/SearchResults/uiComponents/locationsModal.js
@@ -6,24 +6,28 @@ export default function DetailsModal({open, toggleDetailsModal, airlineStock, fa
   let AirlineStockRows 
   if(airlineStock.length > 0){
     AirlineStockRows = _.map(airlineStock, location => {
-      return(
-        <tr>
-          <td>{location.locationName}</td>
-          <td>{location.quantityAvailable}</td>
-        </tr>
-      )
+      if(location.quantityAvailable > 0){
+        return(
+          <tr>
+            <td>{location.locationName}</td>
+            <td>{location.quantityAvailable}</td>
+          </tr>
+        )
+      }
     })
   }
 
   let FactoryStockRows 
   if(factoryStock.length > 0){
     FactoryStockRows = _.map(factoryStock, location => {
-      return(
-        <tr>
-          <td>{location.locationName}</td>
-          <td>{location.quantityAvailable}</td>
-        </tr>
-      )
+      if(location.quantityAvailable > 0){
+        return(
+          <tr>
+            <td>{location.locationName}</td>
+            <td>{location.quantityAvailable}</td>
+          </tr>
+        )
+      }
     })
   }
 
