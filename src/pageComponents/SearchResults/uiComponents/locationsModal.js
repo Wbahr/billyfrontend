5f61ff2e3be738509ad205e493f23031300cbdcb
Bobby Panczer <bobbypanczer@gmail.com>
2019-12-05 13:00:46 +0100

    Only fetch locations on click

--- src/pageComponents/SearchResults/uiComponents/locationsModal.js
+++ src/pageComponents/SearchResults/uiComponents/locationsModal.js
@@ -1,7 +1,9 @@
-import React from 'react'
+import React, {useState, useRef} from 'react'
 import _ from 'lodash'
 import Popup from 'reactjs-popup'
 import styled from 'styled-components'
+import { useQuery, useLazyQuery } from '@apollo/react-hooks';
+import gql from 'graphql-tag';
 
 const Table = styled.table`
   margin: 0 16px;
@@ -24,7 +26,54 @@ const TDWhite = styled.td`
 padding: 4px 24px 4px 8px;
 `
 
-export default function LocationsModal({open, hideLocationsModal, airlineStock, factoryStock}) {
+const QUERY_STOCK_AVAILABILITY = gql`
+  query GetStockAvailability($invMastUid: ID){
+    getStockAvailability(invMastUid: $invMastUid){
+      airlineStocks {
+        companyId
+        itemCode
+        locationId
+        locationName
+        locationType
+        quantityAllocated
+        quantityAvailable
+        quantityFrozen
+        quantityNonPickable
+        quantityOnHand
+        quantityQuarantined
+      }
+      factoryStock {
+        factoryAvailability
+        factoryMessage
+        invMastUid
+        leadTimeDays
+      }
+    }
+  }
+`
+
+export default function LocationsModal({open, hideLocationsModal, invMastUid}) {
+  const [airlineStock, setAirlineStock] = useState([])
+  const [factoryStock, setFactoryStock] = useState([])
+  const searchSent = useRef(false); 
+
+  const [getStockAvailability, { loading, error, data }] = useLazyQuery(QUERY_STOCK_AVAILABILITY, {
+    variables: { invMastUid },
+    onCompleted: data => {
+      setAirlineStock(data.getStockAvailability.airlineStocks)
+      setFactoryStock(data.getStockAvailability.factoryStock)
+    }
+  })
+
+  if(open && !_.isNil(invMastUid) && !searchSent.current){
+    searchSent.current = true
+    getStockAvailability()
+  } else if (!open && searchSent.current || !open && (airlineStock.length > 0 || factoryStock.length > 0)) {
+    searchSent.current = false
+    setAirlineStock([])
+    setFactoryStock([])
+  }
+
   let AirlineStockRows
   if(airlineStock.length > 0){
     AirlineStockRows = _.map(airlineStock, location => {
