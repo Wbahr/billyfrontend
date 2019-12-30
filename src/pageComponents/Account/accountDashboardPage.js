004304f2d8430dba484b50ce2b2f5b1858627105
Bobby Panczer <bobbypanczer@gmail.com>
2019-12-30 22:25:11 +0100

    Update accountDashboardPage.js

--- src/pageComponents/Account/accountDashboardPage.js
+++ src/pageComponents/Account/accountDashboardPage.js
@@ -3,15 +3,13 @@ import PropTypes from 'prop-types'
 import styled from 'styled-components'
 
 export default function AccountDashboard({history}) {
-  const [customerType, setCustomerType] = useState('')
-
-
-
+  // const [customerType, setCustomerType] = useState('')
+  
   return(
     <div>
-      <p>My Account</p>
+      <p>My Account Dashboard</p>
       <div>
-        
+
       </div>
     </div>
   )
