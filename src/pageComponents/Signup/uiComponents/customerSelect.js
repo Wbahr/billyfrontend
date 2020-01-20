647a7b2d3d5ab14dc62a1c323684a91fe0941402
Bobby Panczer <bobbypanczer@gmail.com>
2020-01-20 19:30:31 +0100

    Update Create Account images

--- src/pageComponents/Signup/uiComponents/customerSelect.js
+++ src/pageComponents/Signup/uiComponents/customerSelect.js
@@ -1,5 +1,6 @@
 import React, {useEffect, useState} from 'react'
 import styled from 'styled-components'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 const SignupPageContainer = styled.div`
   display: flex;
@@ -12,11 +13,24 @@ const SignupPageContainer = styled.div`
 `
 
 const Div = styled.div`
+  display: flex;
+  background-image: url(https://www.airlinehyd.com/customer/aihyco/images/Home/CategoryBackground.jpg);
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
   cursor: pointer;
   width: 200px;
   height: 200px;
   margin: 10px;
   border: 1px solid black;
+  border-radius: 2px;
+  p {
+    font-family: Proxima;
+    color: white;
+    margin-top: 16px;
+    margin-botton: 0;
+    font-size: 20px;
+  }
 `
 
 export default function CustomerSelectPage({selectCustomer}) {
@@ -24,9 +38,11 @@ export default function CustomerSelectPage({selectCustomer}) {
   return(
     <SignupPageContainer>
       <Div onClick={()=>selectCustomer('existing')}>
+        <FontAwesomeIcon icon="user" color="white" size="6x"/>
         <p>Existing Customer</p>
       </Div>
       <Div onClick={()=>selectCustomer('new')}>
+        <FontAwesomeIcon icon="user-plus" color="white" size="6x"/>
         <p>New Customer</p>
       </Div>
     </SignupPageContainer>
