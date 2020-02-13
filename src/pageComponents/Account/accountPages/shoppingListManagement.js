02ae53331f4dd731d1359bef45c082983dc66223
Bobby Panczer <bobbypanczer@gmail.com>
2020-02-13 16:41:40 -0500

    Update shoppingListManagement.js

--- src/pageComponents/Account/accountPages/shoppingListManagement.js
+++ src/pageComponents/Account/accountPages/shoppingListManagement.js
@@ -8,17 +8,35 @@ import Select from '../../_common/select'
 
 export default function ShoppingListManagementPage() {
   // const context = useContext(Context);
-  const [defaultShipTo, setDefaultShipTo] = useState(0)
+  const [defaultShipTo, setDefaultShipTo] = useState(null)
+  const [confirmDelete, setConfirmDelete] = useState(false)
 
+
+  function handleDeleteShoppingList(){
+    if(confirmDelete){
+      console.log('deleting shopping list...')
+      setConfirmDelete(false)
+    } else {
+      setConfirmDelete(true)
+    }
+  }
 
   return(
     <div>
-      <p>Select a Shopping List:</p>
+      <p>Saved Shopping Lists:</p>
         <Select
           value={defaultShipTo}
           setValue={setDefaultShipTo}
           options={[{'label': 'Winter Maintenance', 'value': 0},{'label': 'Extruder A5 - 6 Month Replacement Parts', 'value': 1}]}
+          placeholder='Select a Shopping List'
         />
+        {!_.isNil(null) &&
+          <>
+            <p onClick={()=>handleDeleteShoppingList()}>{confirmDelete ? 'Confirm Delete?' : 'Delete this Shopping List'}</p>
+            <button>Copy to Cart</button>
+            <button>Share</button>
+          </>
+        }
     </div>
   )
 }
