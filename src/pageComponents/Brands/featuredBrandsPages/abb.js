5533a58cb3ac7d3e42c9c8b8610d4d70da99edad
Anlinmac <anlinmacmillan@gmail.com>
2020-02-25 08:48:13 -0500

    abb page

--- src/pageComponents/Brands/featuredBrandsPages/abb.js
+++ src/pageComponents/Brands/featuredBrandsPages/abb.js
@@ -1,4 +1,18 @@
-import React, {useState, useEffect, useContext} from 'react'
-import { useParams } from 'react-router-dom'
-import PropTypes from 'prop-types'
+import React, {useState, useContext} from 'react'
+import _ from 'lodash'
 import styled from 'styled-components'
+import Context from '../../../config/context'
+
+export default function Abb() {
+  const context = useContext(Context);
+
+  return(
+    <div>
+      <div>
+        <p>Hello world</p>
+       
+      </div>
+    </div>
+  )
+}
+
