d34fb5819cde0807199a5884f163e379d30a5b0f
Anlinmac <anlinmacmillan@gmail.com>
2020-03-09 11:56:11 -0400

    created acmeElectric.js file

---
+++ src/pageComponents/Brands/minimalBrandsPages/acmeElectric.js
@@ -0,0 +1,6 @@
+import React, { useState, useContext } from 'react'
+import _ from 'lodash'
+import styled from 'styled-components'
+import Context from '../../../config/context'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
