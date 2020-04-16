f6eeb4344751eb46d196c126515ae94e1658c297
Bobby Panczer <bobbypanczer@gmail.com>
2020-04-16 15:45:28 -0400

    Improve Featured Manufacturer section

--- src/pageComponents/Home/uiComponents/featuredManufacturers.js
+++ src/pageComponents/Home/uiComponents/featuredManufacturers.js
@@ -1,8 +1,7 @@
-import React, { Component } from 'react'
-import styled, { keyframes } from 'styled-components'
-import { H2 } from '../../_common/text'
+import React, { useState, useRef, useEffect } from 'react'
+import styled from 'styled-components'
 import SectionHeader from '../../_common/sectionHeader.js'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import FeaturedManufacturerLink from './featuredManufacturerLink'
 
 const Wrapper = styled.div`
   display: flex;
@@ -11,57 +10,70 @@ const Wrapper = styled.div`
   width: 1300px;
   margin: 0 auto 50px;
 `
-const Brandsdiv = styled.div`
-  display: flex;
-  width: 116px;
-  height: 85px;
-  // border: 3px solid #f2f3f4;
-  border-radius: 50%;
-  margin: 25px 35px;
-  align-items: center;
-  text-align: center;
-  &:hover {
-    transition: transform 200ms;
-    transform: scale(1.1);
-  }
-`
-const LogoImg = styled.img`
-  max-width: 100%;
-  padding: 5px;
-`
-const A = styled.a`
-`
 
-export default class CustomSlider extends Component {
+function useInterval(callback, delay) {
+  const savedCallback = useRef();
 
-  render() {
-    return (
-     
-      <>
-        <SectionHeader
-          text='Featured Manufacturers'
-        />
-        <Wrapper>
-          <Brandsdiv><A href="/Brands/featured/smc"><LogoImg src="https://www.airlinehyd.com/customer/aihyco/images/Home/Featured2.png" /></A></Brandsdiv>
-          <Brandsdiv><A href="/Brands/featured/eaton"><LogoImg src="https://www.airlinehyd.com/customer/aihyco/images/Home/Featured3.png" /></A></Brandsdiv>
-          <Brandsdiv><A href="/Brands/featured/hydac"><LogoImg src="https://www.airlinehyd.com/customer/aihyco/images/Home/Featured4.png" /></A></Brandsdiv>
-          <Brandsdiv><A href="/Brands/featured/omron"><LogoImg src="https://www.airlinehyd.com/customer/aihyco/images/Home/Featured5.png" /></A></Brandsdiv>
-          <Brandsdiv><A href="/Brands/featured/abb"><LogoImg src="https://www.airlinehyd.com/customer/aihyco/images/Home/Featured6.png" /></A></Brandsdiv>
-          <Brandsdiv><A href="/Brands/featured/butech"><LogoImg src="https://www.airlinehyd.com/customer/aihyco/images/Home/Featured7.png" /></A></Brandsdiv>
-          <Brandsdiv><A href="/Brands/featured/clippard"><LogoImg src="https://www.airlinehyd.com/customer/aihyco/images/Home/Featured8.png" /></A></Brandsdiv>
-          <Brandsdiv><A href="/Brands/featured/haskel"><LogoImg src="https://www.airlinehyd.com/customer/aihyco/images/Home/Featured9.png" /></A></Brandsdiv>
-          <Brandsdiv><A href="/Brands/featured/lincoln"><LogoImg src="https://www.airlinehyd.com/customer/aihyco/images/Home/Featured10.png" /></A></Brandsdiv>
-          <Brandsdiv><A href="/Brands/featured/orientalmotor"><LogoImg src="https://www.airlinehyd.com/customer/aihyco/images/Home/Featured11.png" /></A></Brandsdiv>
-          <Brandsdiv><A href="/Brands/featured/paccar"><LogoImg src="https://www.airlinehyd.com/customer/aihyco/images/Home/Featured12.png" /></A></Brandsdiv>
-          <Brandsdiv><A href="/Brands/featured/parker"><LogoImg src="https://www.airlinehyd.com/customer/aihyco/images/Home/Featured13.png" /></A></Brandsdiv>
-          <Brandsdiv><A href="/Brands/featured/rittal"><LogoImg src="https://www.airlinehyd.com/customer/aihyco/images/rittal.jpg" /></A></Brandsdiv>
-          <Brandsdiv><A href="/Brands/featured/ross"><LogoImg src="https://www.airlinehyd.com/customer/aihyco/images/Home/Featured15.png" /></A></Brandsdiv>
-          <Brandsdiv><A href="/Brands/featured/schmersal"><LogoImg src="https://www.airlinehyd.com/customer/aihyco/images/Home/Featured16.png" /></A></Brandsdiv>
-          <Brandsdiv><A href="/Brands/featured/phoenixContact"><LogoImg src="https://www.airlinehyd.com/customer/aihyco/images/Home/Featured17.png" /></A></Brandsdiv>
-          <Brandsdiv><A href="/Brands/featured/aventics"><LogoImg src="https://www.airlinehyd.com/customer/aihyco/images/Home/Featured18.png" /></A></Brandsdiv>
-        </Wrapper>
-      </>
-     
-    )
-  };
+  // Remember the latest function.
+  useEffect(() => {
+    savedCallback.current = callback;
+  }, [callback]);
+
+  // Set up the interval.
+  useEffect(() => {
+    function tick() {
+      savedCallback.current();
+    }
+    if (delay !== null) {
+      let id = setInterval(tick, delay);
+      return () => clearInterval(id);
+    }
+  }, [delay]);
+}
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max))
+}
+
+export default function FeaturedManufacturers(){
+  const [r1, setR1] = useState(11)
+  const [r2, setR2] = useState(1)
+
+  useInterval(() => {
+    let random1 = getRandomInt(17)
+    setR1(random1)
+  }, 1200)
+
+  useInterval(() => {
+    let random2 = getRandomInt(17)
+    setR2(random2)
+  }, 1700)
+
+  return (
+    <>
+      <SectionHeader
+        text='Featured Manufacturers'
+      />
+      <Wrapper>
+        <FeaturedManufacturerLink brandPagePath="/Brands/featured/smc" logo="https://www.airlinehyd.com/customer/aihyco/images/Home/Featured2.png" pop={0 === r1 || 0 === r2}/>
+        <FeaturedManufacturerLink brandPagePath="/Brands/featured/eaton" logo="https://www.airlinehyd.com/customer/aihyco/images/Home/Featured3.png" pop={1 === r1 || 1 === r2}/>
+        <FeaturedManufacturerLink brandPagePath="/Brands/featured/hydac" logo="https://www.airlinehyd.com/customer/aihyco/images/Home/Featured4.png" pop={2 === r1 || 2 === r2}/>
+        <FeaturedManufacturerLink brandPagePath="/Brands/featured/omron" logo="https://www.airlinehyd.com/customer/aihyco/images/Home/Featured5.png" pop={3 === r1 || 3 === r2}/>
+        <FeaturedManufacturerLink brandPagePath="/Brands/featured/abb" logo="https://www.airlinehyd.com/customer/aihyco/images/Home/Featured6.png" pop={4 === r1 || 4 === r2}/>
+        <FeaturedManufacturerLink brandPagePath="/Brands/featured/butech" logo="https://www.airlinehyd.com/customer/aihyco/images/Home/Featured7.png" pop={5 === r1 || 5 === r2}/>
+        <FeaturedManufacturerLink brandPagePath="/Brands/featured/clippard" logo="https://www.airlinehyd.com/customer/aihyco/images/Home/Featured8.png" pop={6 === r1 || 6 === r2}/>
+        <FeaturedManufacturerLink brandPagePath="/Brands/featured/haskel" logo="https://www.airlinehyd.com/customer/aihyco/images/Home/Featured9.png" pop={7 === r1 || 7 === r2}/>
+        <FeaturedManufacturerLink brandPagePath="/Brands/featured/lincoln" logo="https://www.airlinehyd.com/customer/aihyco/images/Home/Featured10.png" pop={8 === r1 || 8 === r2}/>
+        <FeaturedManufacturerLink brandPagePath="/Brands/featured/orientalmotor" logo="https://www.airlinehyd.com/customer/aihyco/images/Home/Featured11.png" pop={9 === r1 || 9 === r2}/>
+        <FeaturedManufacturerLink brandPagePath="/Brands/featured/paccar" logo="https://www.airlinehyd.com/customer/aihyco/images/Home/Featured12.png" pop={10 === r1 || 10 === r2}/>
+        <FeaturedManufacturerLink brandPagePath="/Brands/featured/parker" logo="https://www.airlinehyd.com/customer/aihyco/images/Home/Featured13.png" pop={11 === r1 || 11 === r2}/>
+        <FeaturedManufacturerLink brandPagePath="/Brands/featured/rittal" logo="https://www.airlinehyd.com/customer/aihyco/images/Home/Featured14.png" pop={12 === r1 || 12 === r2}/>
+        <FeaturedManufacturerLink brandPagePath="/Brands/featured/ross" logo="https://www.airlinehyd.com/customer/aihyco/images/Home/Featured15.png" pop={13 === r1 || 13 === r2}/>
+        <FeaturedManufacturerLink brandPagePath="/Brands/featured/schmersal" logo="https://www.airlinehyd.com/customer/aihyco/images/Home/Featured16.png" pop={14 === r1 || 14 === r2}/>
+        <FeaturedManufacturerLink brandPagePath="/Brands/featured/phoenixContact" logo="https://www.airlinehyd.com/customer/aihyco/images/Home/Featured17.png" pop={15 === r1 || 15 === r2}/>
+        <FeaturedManufacturerLink brandPagePath="/Brands/featured/aventics" logo="https://www.airlinehyd.com/customer/aihyco/images/Home/Featured18.png" pop={16 === r1 || 16 === r2}/>
+      </Wrapper>
+    </>
+    
+  )
 }
