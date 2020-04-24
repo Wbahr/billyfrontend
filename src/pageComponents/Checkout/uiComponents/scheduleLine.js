50b1dea57a02441cffacaed53b360fd8a0039cc9
Bobby Panczer <bobbypanczer@gmail.com>
2020-04-24 08:56:30 -0400

    Upgrade cart to use itemDetailCache for faster loads

--- src/pageComponents/Checkout/uiComponents/scheduleLine.js
+++ src/pageComponents/Checkout/uiComponents/scheduleLine.js
@@ -1,12 +1,11 @@
-import React, { useState, useEffect, useRef } from 'react'
+import React, { useContext, useEffect, useRef } from 'react'
 import styled from 'styled-components'
 import _ from 'lodash'
-import { useQuery, useLazyQuery } from '@apollo/client';
-import gql from 'graphql-tag';
 import DatePicker from 'react-datepicker'
 import 'react-datepicker/dist/react-datepicker.css'
 import { Field } from 'formik'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import Context from '../../../config/context'
 
 const DivContainer = styled.div`
   display: flex;
@@ -88,66 +87,15 @@ const DivSpacer = styled.div`
   margin: 0 8px;
 `
 
-// const CustomDatePicker = styled.button`
-//   display: flex;
-//   justify-content: center;
-//   width: 110px;
-//   background-color: white;
-//   border: 1px solid lightgrey;
-//   margin: 0 8px;
-// `
-
-const GET_ITEM_BY_ID = gql`
-    query ItemById($itemId: Int){
-      customerPartNumbers(frecno: $itemId){
-        customerPartNumber
-        id
-      }
-      itemDetails(invMastUid: $itemId) {
-          anonPrice
-          invMastUid
-          itemCode
-          itemDesc
-          listPrice
-          mfgPartNo
-          modelCode
-          tariff
-          unitSizeMultiple
-          availability
-          availabilityMessage
-          image {
-            path
-            sequence
-            type
-          }
-      }
-    }
-`
-
 export default function ShippingScheduleItem({item, index}) {
-  const [itemDetails, setItem] = useState(null)
-  const [customerParts, setCustomerParts] = useState([])
   const itemId = parseInt(item.frecno,10)
+  const context = useContext(Context)
+  let displayItem = context.itemDetailCache.find(elem => elem.itemDetails.invMastUid == itemId)
+  const {
+    itemDetails,
+    customerPartNumbers
+  } = displayItem
 
-  const { 
-    loading, 
-    error, 
-    data 
-  } = useQuery(GET_ITEM_BY_ID, {
-    variables: { itemId },
-    onCompleted: result => {
-      if (!_.isNil(result.itemDetails)) {
-        setItem(result.itemDetails)
-      } else {
-        setItem({})
-      }
-      if (!_.isNil(result.customerPartNumbers)) {
-        setCustomerParts(result.customerPartNumbers)
-      } else {
-        setCustomerParts([])
-      }
-    }
-  })
 
   let Content
   if(_.isNil(itemDetails)) {
@@ -167,7 +115,7 @@ export default function ShippingScheduleItem({item, index}) {
     let tomorrowDate = new Date()
     tomorrowDate.setDate(tomorrowDate.getDate() + 1)
 
-    let selectedCustomerPartNumber = customerParts.find(elem => elem.id === item.customerPartNumberId)
+    let selectedCustomerPartNumber = customerPartNumbers.find(elem => elem.id === item.customerPartNumberId)
 
     Content = (
       <DivCard>
