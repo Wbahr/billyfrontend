9dbe0ef4e73dc99568c0756e0ef6561aadcf6998
Bobby Panczer <bobbypanczer@gmail.com>
2020-04-29 11:48:41 -0400

    Working orders table

--- src/pageComponents/Account/myAccountPage.js
+++ src/pageComponents/Account/myAccountPage.js
@@ -1,73 +1,18 @@
-import React, {useState, useEffect, useContext, useMemo} from 'react'
+import React, {useState, useEffect, useContext} from 'react'
 import { useParams } from 'react-router-dom'
 import _ from 'lodash'
-// import styled from 'styled-components'
+import styled from 'styled-components'
 import AccountNavPanel from './accountNavPanel'
 import PropTypes from 'prop-types'
 import AccountDashboard from './accountDashboardPage'
 import OrdersTable from './ordersPages/orders'
 
+const Container = styled.div`
+  display: flex;
+`
+
 export default function AccountManagementPage({history}) {
   let { page } = useParams()
-
-  const data = useMemo(
-    () => [
-      {
-        order_date: 'Hello',
-        order_no: 'World',
-        po_no: '123132',
-        buyer: 'Bpanczer',
-        total: '$12.00',
-        status: 'Completed'
-      },
-      {
-        order_date: 'Hello',
-        order_no: 'World',
-        po_no: '123132',
-        buyer: 'Bpanczer',
-        total: '$12.00',
-        status: 'Completed'
-      },
-      {
-        order_date: 'Hello',
-        order_no: 'World',
-        po_no: '123132',
-        buyer: 'Bpanczer',
-        total: '$12.00',
-        status: 'Completed'
-      }
-    ],
-    [],
-  )
-  const columns = useMemo(
-    () => [
-      {
-        Header: 'Order Data',
-        accessor: 'order_date', // accessor is the "key" in the data
-      },
-      {
-        Header: 'Order #',
-        accessor: 'order_no',
-      },
-      {
-        Header: 'PO #',
-        accessor: 'po_no',
-      },
-      {
-        Header: 'Buyer',
-        accessor: 'buyer',
-      },
-      {
-        Header: 'Total',
-        accessor: 'total',
-      },
-      {
-        Header: 'Status',
-        accessor: 'status',
-      }
-    ],
-    [],
-  )
   
   let AccountPage
   useEffect(() => {
@@ -77,10 +22,10 @@ export default function AccountManagementPage({history}) {
   }, [page])
 
   return(
-    <>
+    <Container>
       <AccountNavPanel history={history}/>
-      <OrdersTable data={data} columns={columns}/>
-    </>
+      <OrdersTable/>
+    </Container>
   )
 }
 
--- src/pageComponents/Account/ordersPages/orders.js
+++ src/pageComponents/Account/ordersPages/orders.js
@@ -1,16 +1,176 @@
 import React, { useMemo } from 'react'
-import { useTable } from 'react-table'
+import styled from 'styled-components'
+import { useTable, usePagination} from 'react-table'
 
-export default function Orders({ columns, data }) {
+const TableContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`
+
+export default function Orders() {
+
+  const data = useMemo(
+    () => [
+      {
+        order_date: 'Hello',
+        order_no: 'World',
+        po_no: '123132',
+        buyer: 'Bpanczer',
+        total: '$12.00',
+        status: 'Completed'
+      },
+      {
+        order_date: 'Hello',
+        order_no: 'World',
+        po_no: '123132',
+        buyer: 'Bpanczer',
+        total: '$12.00',
+        status: 'Completed'
+      },
+      {
+        order_date: 'Hello',
+        order_no: 'World',
+        po_no: '123132',
+        buyer: 'Bpanczer',
+        total: '$12.00',
+        status: 'Completed'
+      },
+      {
+        order_date: 'Hello',
+        order_no: 'World',
+        po_no: '123132',
+        buyer: 'Bpanczer',
+        total: '$12.00',
+        status: 'Completed'
+      },
+      {
+        order_date: 'Hello',
+        order_no: 'World',
+        po_no: '123132',
+        buyer: 'Bpanczer',
+        total: '$12.00',
+        status: 'Completed'
+      },
+      {
+        order_date: 'Hello',
+        order_no: 'World',
+        po_no: '123132',
+        buyer: 'Bpanczer',
+        total: '$12.00',
+        status: 'Completed'
+      },
+      {
+        order_date: 'Hello',
+        order_no: 'World',
+        po_no: '123132',
+        buyer: 'Bpanczer',
+        total: '$12.00',
+        status: 'Completed'
+      },
+      {
+        order_date: 'Hello',
+        order_no: 'World',
+        po_no: '123132',
+        buyer: 'Bpanczer',
+        total: '$12.00',
+        status: 'Completed'
+      },
+      {
+        order_date: 'Hello',
+        order_no: 'World',
+        po_no: '123132',
+        buyer: 'Bpanczer',
+        total: '$12.00',
+        status: 'Completed'
+      },
+      {
+        order_date: 'Hello',
+        order_no: 'World',
+        po_no: '123132',
+        buyer: 'Bpanczer',
+        total: '$12.00',
+        status: 'Completed'
+      },
+      {
+        order_date: 'Hello',
+        order_no: 'World',
+        po_no: '123132',
+        buyer: 'Bpanczer',
+        total: '$12.00',
+        status: 'Completed'
+      },
+      {
+        order_date: 'Hello',
+        order_no: 'World',
+        po_no: '123132',
+        buyer: 'Bpanczer',
+        total: '$12.00',
+        status: 'Completed'
+      }
+    ],
+    [],
+  )
+  const columns = useMemo(
+    () => [
+      {
+        Header: 'Order Date',
+        accessor: 'order_date', // accessor is the "key" in the data
+      },
+      {
+        Header: 'Order #',
+        accessor: 'order_no',
+      },
+      {
+        Header: 'PO #',
+        accessor: 'po_no',
+      },
+      {
+        Header: 'Buyer',
+        accessor: 'buyer',
+      },
+      {
+        Header: 'Total',
+        accessor: 'total',
+      },
+      {
+        Header: 'Status',
+        accessor: 'status',
+      }
+    ],
+    [],
+  )
   const {
     getTableProps,
     getTableBodyProps,
     headerGroups,
-    rows,
+    rows, //all rows
     prepareRow,
-  } = useTable({ columns, data })
+    page, // current page in rows
+    canPreviousPage,
+    canNextPage,
+    pageOptions,
+    pageCount,
+    gotoPage,
+    nextPage,
+    previousPage,
+    setPageSize,
+    state: { pageIndex, pageSize },
+  } = useTable(    
+    {
+      columns,
+      data,
+      initialState: { pageIndex: 0 },
+    },
+    usePagination
+  )
 
   return(
+    <TableContainer>
+    <h4>Orders</h4>
+    <input placeholder='Search PO#, Order #, Item ID'></input>
+    <select>
+      <option>All Orders</option>
+    </select>
     <table {...getTableProps()}>
       <thead>
         {headerGroups.map(headerGroup => (
@@ -24,7 +184,7 @@ export default function Orders({ columns, data }) {
         ))}
       </thead>
       <tbody {...getTableBodyProps()}>
-        {rows.map(row => {
+        {page.map(row => {
           prepareRow(row)
           return (
             <tr {...row.getRowProps()}>
@@ -40,5 +200,54 @@ export default function Orders({ columns, data }) {
         })}
       </tbody>
     </table>
+          {/* 
+        Pagination can be built however you'd like. 
+        This is just a very basic UI implementation:
+      */}
+      <div>
+        <button onClick={() => gotoPage(0)} disabled={!canPreviousPage}>
+          {'<<'}
+        </button>{' '}
+        <button onClick={() => previousPage()} disabled={!canPreviousPage}>
+          {'<'}
+        </button>{' '}
+        <button onClick={() => nextPage()} disabled={!canNextPage}>
+          {'>'}
+        </button>{' '}
+        <button onClick={() => gotoPage(pageCount - 1)} disabled={!canNextPage}>
+          {'>>'}
+        </button>{' '}
+        <span>
+          Page{' '}
+          <strong>
+            {pageIndex + 1} of {pageOptions.length}
+          </strong>{' '}
+        </span>
+        <span>
+          | Go to page:{' '}
+          <input
+            type="number"
+            defaultValue={pageIndex + 1}
+            onChange={e => {
+              const page = e.target.value ? Number(e.target.value) - 1 : 0
+              gotoPage(page)
+            }}
+            style={{ width: '100px' }}
+          />
+        </span>{' '}
+        <select
+          value={pageSize}
+          onChange={e => {
+            setPageSize(Number(e.target.value))
+          }}
+        >
+          {[10, 20, 30, 40, 50].map(pageSize => (
+            <option key={pageSize} value={pageSize}>
+              Show {pageSize}
+            </option>
+          ))}
+        </select>
+        </div>
+      </TableContainer>
   )
 }
