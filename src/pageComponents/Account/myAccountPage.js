3034f1338387d6d983e62e9c44e6635fa57f953f
Bobby Panczer <bobbypanczer@gmail.com>
2020-04-29 15:07:23 -0400

    Base pages for most of My Account section

--- src/pageComponents/Account/myAccountPage.js
+++ src/pageComponents/Account/myAccountPage.js
@@ -6,6 +6,9 @@ import AccountNavPanel from './accountNavPanel'
 import PropTypes from 'prop-types'
 import AccountDashboard from './accountDashboardPage'
 import OrdersTable from './ordersPages/orders'
+import ItemOrdersTable from './ordersPages/item-orders'
+import OpenQuotesTable from './quotePages/openQuotes'
+import InvoicesTable from './invoicePages/invoices'
 
 const Container = styled.div`
   display: flex;
@@ -14,7 +17,7 @@ const Container = styled.div`
 export default function AccountManagementPage({history}) {
   const [pageComponent, setPageComponent] = useState()
   let { page } = useParams()
-  
+
   useEffect(() => {
     switch(page){
       case 'dashboard':
@@ -23,8 +26,17 @@ export default function AccountManagementPage({history}) {
       case 'my-orders':
         setPageComponent(<OrdersTable history={history}/>)
         break;
+      case 'my-ordered-items':
+        setPageComponent(<ItemOrdersTable history={history}/>)
+        break;
+      case 'open-quotes':
+        setPageComponent(<OpenQuotesTable history={history}/>)
+        break;
+      case 'invoices':
+        setPageComponent(<InvoicesTable history={history}/>)
+        break;
       default:
-        setPageComponent(<AccountDashboard history={history}/>)
+        setPageComponent(<OrdersTable history={history}/>)
         break;
     }
   }, [page])
