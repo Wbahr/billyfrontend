a997223d04c7812ad689f9b7788a1f204aec45f9
Anlinmac <anlinmacmillan@gmail.com>
2020-05-05 07:42:09 -0400

    created component

--- src/pageComponents/About/aboutSubPages/transactionalServicesPage.js
+++ src/pageComponents/About/aboutSubPages/transactionalServicesPage.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import styled from 'styled-components'
 import _ from 'lodash'
+import Header from '../uiComponents/Header'
+import PageContainer from '../uiComponents/PageContainer'
 
+const Container = styled.div`
+  max-width: 1300px;
+  margin: 50px auto;
+`
 export default function transactionalServicesPage() {
-    return (
-        <div>
-            Hi
-        </div>
+    return (     
+        <Container>
+            <Header text="Transactional Services" />
+        </Container>
     )
 }
---
+++ src/pageComponents/About/uiComponents/PageContainer.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import _ from 'lodash'
+import styled from 'styled-components'
+
+const Container = styled.div`
+  max-width: 1300px;
+  margin: 50px auto;
+`
+export default function PageContainer() {
+    return (
+   
+        <Container>
+            
+        </Container>
+     
+    )
+}
