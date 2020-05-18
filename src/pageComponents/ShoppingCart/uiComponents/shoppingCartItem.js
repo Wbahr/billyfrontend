86b84ea10b3e5c6a32e468420668cc4ea16a79d7
Bobby Panczer <bobbypanczer@gmail.com>
2020-05-18 10:22:46 -0400

    372 lint errors

--- src/pageComponents/ShoppingCart/uiComponents/shoppingCartItem.js
+++ src/pageComponents/ShoppingCart/uiComponents/shoppingCartItem.js
@@ -10,164 +10,152 @@ import {CopyToClipboard} from 'react-copy-to-clipboard'
 import NumberFormat from 'react-number-format'
 
 const DivContainer = styled.div`
-  display: flex;
-  border-top: 2px whitesmoke solid;
-  border-bottom: 2px whitesmoke solid;
-  padding: 8px 16px;
-  margin: 8px 0;
-  min-height: 135px;
-  background-color: white;
+	display: flex;
+	border-top: 2px whitesmoke solid;
+	border-bottom: 2px whitesmoke solid;
+	padding: 8px 16px;
+	margin: 8px 0;
+	min-height: 135px;
+	background-color: white;
 `
 
 const DivRow = styled.div`
-  display: flex;
-  margin-top: 8px;
+	display: flex;
+	margin-top: 8px;
 `
 
 const DivItem = styled.div`
-  display: flex;
-  flex-direction: column;
+	display: flex;
+	flex-direction: column;
 `
 
 const DivCard = styled.div`
-  display: flex;
-  align-items: center;
-  width: 100%;
+	display: flex;
+	align-items: center;
+	width: 100%;
 `
 
 const DivQuantity = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  width: 100%;
-`
-
-const DivDivide = styled.div`
-  display: flex;
-  cursor: pointer;
-  align-items: center;
-  justify-content: center;
-  background-color: #328EFC;
-  border-radius: 50px;
-  height: 20px;
-  width: 20px;
-  opacity: 0.5;
+	display: flex;
+	align-items: center;
+	justify-content: space-between;
+	width: 100%;
 `
 
 const DivRemove = styled.div`
-  cursor: pointer;
-  display: flex;
-  width: auto;
-  margin: auto 12px;
-  align-items: center;
+	cursor: pointer;
+	display: flex;
+	width: auto;
+	margin: auto 12px;
+	align-items: center;
 `
 
 const DivSplitLine = styled(DivRemove)`
-  padding: 0 3px;
-  // border: 1px solid #328EFC;
-  margin: 0;
-  border-radius: 50px;
-  color: #328EFC;
-  height: 20px;
-  font-size: 12px;
-  // padding-left: 8px;
-  font-weight: 600;
+	padding: 0 3px;
+	// border: 1px solid #328EFC;
+	margin: 0;
+	border-radius: 50px;
+	color: #328EFC;
+	height: 20px;
+	font-size: 12px;
+	// padding-left: 8px;
+	font-weight: 600;
 `
 
 const DivMove = styled.div`
-  cursor: move;
-  display: flex;
-  height: 100%;
-  align-items: center;
-  padding: 0 12px;
+	cursor: move;
+	display: flex;
+	height: 100%;
+	align-items: center;
+	padding: 0 12px;
 `
 
 const DivCol1 = styled.div`
-  display: flex;
-  width: 100px;
+	display: flex;
+	width: 100px;
 `
 
 const DivCol2 = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-  width: 300px;
-  height: 100%;
-  margin-right: 50px;
-  p {
-    font-size: 16px;
-    margin: 0;
-  }
+	display: flex;
+	flex-direction: column;
+	align-items: flex-start;
+	width: 300px;
+	height: 100%;
+	margin-right: 50px;
+	p {
+		font-size: 16px;
+		margin: 0;
+	}
 `
 
 const DivCol3 = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-  flex-grow: 99;
+	display: flex;
+	flex-direction: column;
+	align-items: flex-start;
+	flex-grow: 99;
 `
 
 const Img = styled.img`
-  margin: 0 4px;
+	margin: 0 4px;
 `
 
 const DivTotalPrice = styled.div`
-  display: flex;
-  width: 150px;
-  align-items: center;
-  justify-items: flex-end;
-  p {
-    text-align: right;
-    font-size: 20px;
-    margin: 0 20px 0 auto;
-    font-weight: 600;
-  }
+	display: flex;
+	width: 150px;
+	align-items: center;
+	justify-items: flex-end;
+	p {
+		text-align: right;
+		font-size: 20px;
+		margin: 0 20px 0 auto;
+		font-weight: 600;
+	}
 `
 
 const Label = styled.label`
-  margin: 0;
-  font-size: 12px;
-  font-style: italic;
+	margin: 0;
+	font-size: 12px;
+	font-style: italic;
 `
 
 const Peach = styled.p`
-  margin: 0;
-  padding-right: 8px;
+	margin: 0;
+	padding-right: 8px;
 `
 
 const DivEditPrice = styled.div`
-  cursor: pointer;
+	cursor: pointer;
 `
 
 const P1 = styled.p`
-  cursor: pointer;
-  font-size: 16px;
-  font-weight: 600;
+	cursor: pointer;
+	font-size: 16px;
+	font-weight: 600;
 `
 
 const TextRow = styled.div`
-  display: flex;
+	display: flex;
 `
 
 const P2 = styled.p`
-  cursor: pointer;
-  color: grey;
-  font-size: 12px !important;
-  padding: 0 2px;
+	cursor: pointer;
+	color: grey;
+	font-size: 12px !important;
+	padding: 0 2px;
 `
 
 const P3 = styled.p`
-  color: black;
-  font-size: 12px !important;
+	color: black;
+	font-size: 12px !important;
 `
 
 const GET_UPDATED_CUSTOMER_PART_NUMBERS = gql`
-  query ItemById($itemId: Int){
-    customerPartNumbers(frecno: $itemId){
-      customerPartNumber
-      id
-    }
-  }
+	query ItemById($itemId: Int){
+		customerPartNumbers(frecno: $itemId){
+			customerPartNumber
+			id
+		}
+	}
 `
 
 export default function ShoppingCartItem({item, displayItem, index, showSplitLineModal, showFactoryStockModal, showEditPriceModal, showCustomerPartModal, handleSetModalData}) {
@@ -291,14 +279,14 @@ export default function ShoppingCartItem({item, displayItem, index, showSplitLin
 							<option value="-1">Create Part#</option>
 						</select>
 						{ selectedCustomerPartNumber !== '0' &&
-              <div style={{'marginLeft': '8px', 'cursor': 'pointer'}} onClick={()=>clearCustomerPartNumber()}>
-              	<FontAwesomeIcon icon="times" color="grey" />
-              </div>
+							<div style={{'marginLeft': '8px', 'cursor': 'pointer'}} onClick={()=>clearCustomerPartNumber()}>
+								<FontAwesomeIcon icon="times" color="grey" />
+							</div>
 						}
 					</TextRow>
 					<DivRow>
 						<Context.Consumer>
-							{({ updateItem, cart }) => (
+							{({ cart }) => (
 								<P3>Availability: {itemDetails.availability} {(cart[index].quantity > itemDetails.availability)&& '| '  + itemDetails.availabilityMessage }</P3>
 							)}
 						</Context.Consumer>
@@ -329,10 +317,10 @@ export default function ShoppingCartItem({item, displayItem, index, showSplitLin
 							<DivRow>
 								<Context.Consumer>
 									{({ cart }) => (
-                    <>
-                      <Peach>{_.isNil(cart[index].itemUnitPriceOverride) ? <NumberFormat value={itemDetails.listPrice} displayType={'text'} thousandSeparator={true} prefix={'$'} decimalScale={2} fixedDecimalScale/> : <NumberFormat value={cart[index].itemUnitPriceOverride} displayType={'text'} thousandSeparator={true} prefix={'$'} decimalScale={2} fixedDecimalScale/>/each}</Peach>
-                      <DivEditPrice onClick={()=>handleShowModal('edit-price')}><FontAwesomeIcon icon="pencil-alt" color={!_.isNil(cart[index].itemUnitPriceOverride) ? '#328EFC' : 'grey'} /></DivEditPrice>
-                    </>
+										<>
+											<Peach>{_.isNil(cart[index].itemUnitPriceOverride) ? <NumberFormat value={itemDetails.listPrice} displayType={'text'} thousandSeparator={true} prefix={'$'} decimalScale={2} fixedDecimalScale/> : <NumberFormat value={cart[index].itemUnitPriceOverride} displayType={'text'} thousandSeparator={true} prefix={'$'} decimalScale={2} fixedDecimalScale/>/each}</Peach>
+											<DivEditPrice onClick={()=>handleShowModal('edit-price')}><FontAwesomeIcon icon="pencil-alt" color={!_.isNil(cart[index].itemUnitPriceOverride) ? '#328EFC' : 'grey'} /></DivEditPrice>
+										</>
 									)}
 								</Context.Consumer>
 							</DivRow>
@@ -367,11 +355,11 @@ export default function ShoppingCartItem({item, displayItem, index, showSplitLin
 				</DivCol3>
 				<Context.Consumer>
 					{({ removeItem }) => (
-                <>
-                  <DivRemove onClick={()=>removeItem(index)} alt='remove-item'>
-                  	<FontAwesomeIcon icon="times-circle" color="lightgrey"/>
-                  </DivRemove>
-                </>
+								<>
+									<DivRemove onClick={()=>removeItem(index)} alt='remove-item'>
+										<FontAwesomeIcon icon="times-circle" color="lightgrey"/>
+									</DivRemove>
+								</>
 					)}
 				</Context.Consumer>
 			</DivCard>
--- src/pageComponents/ShoppingCart/uiComponents/splitLineModal.js
+++ src/pageComponents/ShoppingCart/uiComponents/splitLineModal.js
@@ -1,5 +1,4 @@
 import React, { useState } from 'react'
-import _ from 'lodash'
 import Modal from '../../_common/modal'
 import styled from 'styled-components'
 import Context from '../../../config/context'
