7634a6f8a935e8f406245333eb40cf5ed12ad7fc
Saige Mehl <smmehl0311@gmail.com>
2020-06-23 10:46:26 -0500

    remove logs

--- src/config/provider.js
+++ src/config/provider.js
@@ -187,7 +187,6 @@ export default function Provider(props) {
 	const [handleGetItemPrices] = useLazyQuery(GET_ITEM_PRICE, {
 		fetchPolicy: 'no-cache',
 		onCompleted: data => {
-			console.log('handleGetItemPrices', [...data.getItemPrices, ...itemPrices].filter(distinct))
 			setItemPrices([...data.getItemPrices, ...itemPrices].filter(distinct))
 		}
 	})
@@ -195,7 +194,6 @@ export default function Provider(props) {
 	const [handleGetItemAvailabilities] = useLazyQuery(GET_ITEM_AVAILABILITY, {
 		fetchPolicy: 'no-cache',
 		onCompleted: data => {
-			console.log('handleGetItemAvailabilities', [...data.itemAvailability, ...itemAvailabilities].filter(distinct))
 			setItemAvailabilities([...data.itemAvailability, ...itemAvailabilities].filter(distinct))
 		}
 	})
