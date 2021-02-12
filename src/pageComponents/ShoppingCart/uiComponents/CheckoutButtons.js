86b6272bcd11a1e73914391e2259b1902ec99d9f
John George <JGeorge@airlinehyd.com>
2021-02-12 16:45:41 -0500

    Create CheckoutButtons component

---
+++ src/pageComponents/ShoppingCart/uiComponents/CheckoutButtons.js
@@ -0,0 +1,81 @@
+import React, { useContext } from 'react'
+import styled from 'styled-components'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import Context from '../../../setup/context'
+
+const DivButtonContainer = styled.div `
+	margin: auto auto 0 auto;
+`
+
+const DivCheckoutButton = styled.div`
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	background-color: #db1633;
+	background-image: linear-gradient(to top left, #950f23, #DB1633);
+	color: white;
+	padding: 8px 16px;
+	cursor: pointer;
+	width: 250px;
+	margin: 8px 0;
+	box-shadow: 1px 1px 2px #000;
+	
+	p {
+		margin: 0;
+		margin-left: 8px;
+		font-size: 18px;
+		font-weight: 500;
+	}
+
+    &[disabled] {
+        cursor: default;
+        background-image: linear-gradient(to top left, grey, darkgrey);
+    }
+`
+
+const DivQuoteButton = styled(DivCheckoutButton)`
+	background-image: none;
+	background-color: #535353;
+`
+
+const CheckoutButtons = ({ history }) => {
+
+    const {
+        userInfo,
+        cart
+    } = useContext(Context)
+
+    return (
+        <>
+            {cart?.length > 0 && (
+                <DivButtonContainer>
+                    {
+                        (userInfo?.isAirlineEngineerUser) 
+                            ? (
+                                <DivCheckoutButton disabled>
+                                    <FontAwesomeIcon icon="lock" color="white"/>
+                                    <p>Engineer User</p>
+                                </DivCheckoutButton>
+                            )
+                            : (
+                                <DivCheckoutButton onClick={() => history.push('/checkout')}>
+                                    <FontAwesomeIcon icon="lock" color="white"/>
+                                    <p>Start Secure Checkout</p>
+                                </DivCheckoutButton>
+                            )
+                    }
+                    {
+                        (userInfo?.isImpersonatorUser) && (
+                            <DivQuoteButton onClick={() => history.push('/create-quote')}>
+                                <FontAwesomeIcon icon='file-invoice-dollar' color="white"/>
+                                <p>Create a Quote</p>
+                            </DivQuoteButton>
+                        )
+                    }
+                </DivButtonContainer>
+            )}
+        </>
+    )
+}
+
+export default CheckoutButtons
--- src/pageComponents/ShoppingCart/uiComponents/subtotalBox.js
+++ src/pageComponents/ShoppingCart/uiComponents/subtotalBox.js
@@ -1,10 +1,9 @@
 import React, { useContext } from 'react'
 import styled from 'styled-components'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { DebounceInput } from 'react-debounce-input'
 import Context from '../../../setup/context'
 import NumberFormat from 'react-number-format'
-import { AIRLINE_ENGINEER_USER } from 'pageComponents/_common/constants/UserTypeConstants'
+import CheckoutButtons from './CheckoutButtons'
 
 const Container = styled.div`
 	display: flex;
@@ -22,33 +21,8 @@ const Div = styled.div`
 	align-items: flex-end;
 `
 
-const DivCheckoutButton = styled.div`
-	display: flex;
-	align-items: center;
-	justify-content: center;
-	background-color: #db1633;
-	background-image: linear-gradient(to top left, #950f23, #DB1633);
-	color: white;
-	padding: 8px 16px;
-	cursor: pointer;
-	width: 250px;
-	margin: 4px 0;
-	box-shadow: 1px 1px 2px #000;
-	p {
-		margin: 0;
-		margin-left: 8px;
-		font-size: 18px;
-		font-weight: 500;
-	}
-`
-
-const DivQuoteButton = styled(DivCheckoutButton)`
-	background-image: none;
-	background-color: #535353;
-`
-
 export default function SubtotalBox({ history }) {
-    const { cart, cartPricing, updateOrderNotes, orderNotes, userInfo } = useContext(Context)
+    const { cartPricing, updateOrderNotes, orderNotes } = useContext(Context)
 
     const subtotal = cartPricing.state === 'loading'
         ? 'Calculating...'
@@ -78,22 +52,7 @@ export default function SubtotalBox({ history }) {
             <Div>
                 <h5>Subtotal: {subtotal}</h5>
 				
-                { cart?.length > 0 && (
-                    <>
-                        {userInfo?.role !== AIRLINE_ENGINEER_USER && (
-                            <DivCheckoutButton onClick={() => history.push('/checkout')}>
-                                <FontAwesomeIcon icon="lock" color="white"/>
-                                <p>Start Secure Checkout</p>
-                            </DivCheckoutButton>
-                        )}
-                        {userInfo?.isEmployee && (
-                            <DivQuoteButton onClick={() => history.push('/create-quote')}>
-                                <FontAwesomeIcon icon='file-invoice-dollar' color="white"/>
-                                <p>Create a Quote</p>
-                            </DivQuoteButton>
-                        )}
-                    </>
-                )}
+                <CheckoutButtons history={history} />
             </Div>
         </Container>
     )
