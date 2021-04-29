11cfaca601d8003285aa73af5ef1fb6b91f217bd
Anlinmac <anlinmacmillan@gmail.com>
2021-04-29 15:35:32 -0400

    updated Airline's blog

--- src/pageComponents/Home/uiComponents/technicallySpeaking.js
+++ src/pageComponents/Home/uiComponents/technicallySpeaking.js
@@ -5,20 +5,25 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 const Container = styled.div`
 		display: flex;
-		flex-direction: row;
 		flex-wrap: wrap;
+        justify-content: space-evenly;
 `
 const Row = styled.div`
 		display: flex;
 		flex-wrap: wrap;
 		flex-direction: row;
 		background-color: #f2f3f4;
-		flex: 1;
 		margin: 20px;
+		justify-content: center;
+        max-width: 300px;
+        width: 100%;
+        box-shadow: 0 1px 1px rgb(0 0 0 / 11%), 0 2px 2px rgb(0 0 0 / 11%), 0 4px 4px rgb(0 0 0 / 11%), 0 8px 8px rgb(0 0 0 / 11%), 0 16px 16px rgb(0 0 0 / 11%), 0 32px 32px rgb(0 0 0 / 11%);
+`
+const ImgDiv = styled.div`
 `
 const Img = styled.img`
-		width: 305px;
-		height: 290px;
+		max-width: 100%; 
+		object-fit: cover;
 		@media (max-width: 345px) {
 			width: 100%;
 			height: auto;
@@ -26,18 +31,17 @@ const Img = styled.img`
 `
 const Details = styled.div`
 		margin: 0;
-		font-family: Verdana;
-		font-size: 22px;
+		font-size: 18px;
 		@media (max-width: 800px) {
-				font-size: 25px;
+				font-size: 12px;
 			} 
 `
 const TextDiv = styled.div`
 		display: flex;
-		padding: 20px 10px;
+		padding: 10px;
 		align-items: center;
 `
-const Button = styled.button`
+const Button = styled.a`
 		background: #0058a3;
 		color: #fff;
 		border: 0;
@@ -45,17 +49,21 @@ const Button = styled.button`
 		user-select: none;
 		padding: 10px;
 		&:hover {
+			text-decoration: none;
 			color: white;
-			border-radius: 50px;
 			border-color: #494949 !important;
-			transition: all 0.3s ease 0s;
 		}
 		@media (max-width: 800px) {
-			font-size: 20px;
+			font-size: 14px;
+            padding: 5px 10px;
 		 }
 		`
 const ButtonDiv = styled.div`
-		padding: 30px 0;
+		padding: 20px 0;
+		transition: 0.2s;
+		&:hover {
+			transform: translate(0, -4px);
+		}
 		@media (max-width: 800px) {
 			padding: 15px 0;
 		 }
@@ -64,7 +72,7 @@ const SeeAll = styled.div`
 		padding: 17px;
 		font-size: 22px;
 		@media (max-width: 800px) {
-			 font-size: 25px;
+			 font-size: 20px;
 		}
 `
 const A = styled.a`
@@ -72,45 +80,71 @@ const A = styled.a`
 `
 const Wrapper = styled.div`
 		margin-bottom: 50px;
+		flex-wrap: wrap;
+        // max-width: 1500px;
+        width: 100%;
+        margin: 0 auto;
 `
+
+
 export default function TechnicallySpeaking() {
 
     return (
         <>
-            <SectionHeader text='Technically Speaking'/>
+            <SectionHeader text='Technically Speaking' />
             <Wrapper>
                 <Container>
                     <Row>
-                        <div>
-                            <Img src="https://world-nuclear-news.org/originalimages/uploads/1/MHI-steam_generators.jpg" />
-                        </div>
-						
-                        <TextDiv> 
-                            <Details >Lorem Ipsum has been the industry's standard dummy text 
-                                <ButtonDiv> <Button>Watch More</Button></ButtonDiv>
-                            </Details> 
+                        <ImgDiv>
+                            <Img src="https://airlinemedia.airlinehyd.com/Static_pages/blog/thumbnail/case%20lifters%20website%20thumb.png" />
+                        </ImgDiv>
+                        <TextDiv>
+                            <Details>Electric Case Lifters: Helping Employee Safety, Productivity & Happiness
+                                <ButtonDiv>
+                                    <Button href="https://airlinehyd-8466844.hs-sites.com/airline-hydraulics-blog/electric-case-lifters" target="_blank">Read More</Button>
+                                </ButtonDiv>
+                            </Details>
                         </TextDiv>
                     </Row>
-					
                     <Row>
-                        <div>
-                            <Img src="https://www.power-eng.com/wp-content/uploads/content/dam/pe/online-articles/2016/04/LNG-solution.jpg" />
-                        </div>
-						
+                        <ImgDiv>
+                            <Img src="https://airlinemedia.airlinehyd.com/Static_pages/blog/thumbnail/gas%20booster%20controls%20website%20thumb.png" />
+                        </ImgDiv>
                         <TextDiv>
-                            <Details>
-                                Lorem Ipsum has been the industry's standard dummy text
+                            <Details>How to Set Gas Booster Controls: A Technical Guide for Control Selection & Plumbing
                                 <ButtonDiv>
-                                    <Button>Read More</Button>
+                                    <Button href="https://airlinehyd-8466844.hs-sites.com/airline-hydraulics-blog/how-to-set-gas-booster-controls" target="_blank">Read More</Button>
+                                </ButtonDiv>
+                            </Details>
+                        </TextDiv>
+                    </Row>
+                    <Row>
+                        <ImgDiv>
+                            <Img src="https://airlinemedia.airlinehyd.com/Static_pages/blog/thumbnail/smart%20elements%20website%20thumb.png" />
+                        </ImgDiv>
+                        <TextDiv>
+                            <Details>What you need to know about Phoenix Contact's new Axioline Smart Elements
+                                <ButtonDiv> <Button href="https://airlinehyd-8466844.hs-sites.com/airline-hydraulics-blog/axioline-smart-elements" target="_blank">Read More</Button></ButtonDiv>
+                            </Details>
+                        </TextDiv>
+                    </Row>
+                    <Row>
+                        <ImgDiv>
+                            <Img src="https://airlinemedia.airlinehyd.com/Static_pages/blog/thumbnail/moisture%20website%20thumb.png" />
+                        </ImgDiv>
+                        <TextDiv>
+                            <Details>How to Prevent & Remove Moisture in Compressed Air Lines
+                                <ButtonDiv>
+                                    <Button href="https://airlinehyd-8466844.hs-sites.com/airline-hydraulics-blog/air-line-moisture-removal" target="_blank">Read More</Button>
                                 </ButtonDiv>
                             </Details>
                         </TextDiv>
                     </Row>
                 </Container>
-				
+
                 <div>
                     <SeeAll>
-                        <A href="#">See all videos and articles <FontAwesomeIcon icon='arrow-circle-right' size='1x' /></A>
+                        <A href="https://airlinehyd-8466844.hs-sites.com/airline-hydraulics-blog" target="_blank">See All Blog Posts <FontAwesomeIcon icon='arrow-circle-right' size='1x' /></A>
                     </SeeAll>
                 </div>
             </Wrapper>
