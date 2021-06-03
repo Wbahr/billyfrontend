7989653199284a4156264a54abb45ee460a3b5c0
Anlinmac <anlinmacmillan@gmail.com>
2021-06-03 14:59:10 -0400

    created new component for the button

--- src/layoutComponents/headerfooterLayout/footer.js
+++ src/layoutComponents/headerfooterLayout/footer.js
@@ -165,7 +165,16 @@ const ACallUs = styled.a`
 	font-weight: 600;
 	margin: 0 0 0 8px;
 `
-
+const Button = styled.button`
+	cursor: pointer;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	background-color: transparent;
+	padding: 4px 12px;
+	border: none;
+	margin: 5px 0;
+`
 
 export default function FooterComponent() {
     
@@ -225,7 +234,7 @@ export default function FooterComponent() {
         <>
             <Newsletter>
                 <NewsletterLabel>Subscribe to Our Blog</NewsletterLabel>
-                <ButtonBlack id="hubspotForm"></ButtonBlack>
+                <Button id="hubspotForm"></Button>
             </Newsletter>
 			
             <ContainerTop>
